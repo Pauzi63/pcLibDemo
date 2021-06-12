a54171cb6ada17a8105e60d831f298db6b2b4a32
Pauzenberger Christian <office@pauzenberger.at>
2021-06-12 10:23:05 +0200

    LowerCase

---
+++ src/p5coreLib/commonPages/DrawerContent/AppMenu.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import AppMenuItem from "./AppMenuItem";
+import { appMenuItems } from "../../../routes/appMenuItems";
+import globals from "../../../p5Lib/globals";
+
+const drawerWidth = globals.drawerWidth;
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    appMenu: {
+      width: "100%",
+    },
+    navList: {
+      width: drawerWidth,
+    },
+    menuItem: {
+      width: drawerWidth,
+    },
+    menuItemIcon: {
+      color: globals.companyIconColor,
+    },
+  })
+);
+
+const AppMenu: React.FC = () => {
+  const classes = useStyles();
+
+  return (
+    <List component="nav" className={classes.appMenu} disablePadding>
+      {/* <AppMenuItem {...appMenuItems[0]} /> */}
+      {appMenuItems.map((item, index) => (
+        <AppMenuItem {...item} key={index} />
+      ))}
+    </List>
+  );
+};
+
+export default AppMenu;
