8bb8879449fa8b9d28cbb46b659332aaef5876e4
Christian Pauzenberger <christian.pauzenberger@kremsmueller.com>
2021-08-26 09:46:04 +0200

    krmTheme Korrektur

--- src/p5Lib/styles/krmTheme.tsx
+++ src/p5Lib/styles/krmTheme.tsx
@@ -4,7 +4,7 @@ import { red, grey } from "@material-ui/core/colors";
 // durch UserLayoutSettings Context ersetzen?
 import useDarkMode from "../../p5coreLib/utils/useDarkMode";
 
-const useThemes = () => {
+const KrmTheme = () => {
   const { darkMode } = useDarkMode();
   return responsiveFontSizes(
     createMuiTheme({
